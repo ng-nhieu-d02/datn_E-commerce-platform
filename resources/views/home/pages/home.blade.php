f3848ba1fc378d3b3dd440a54146349856a32b12
nguyennhieu1507 <nhieunxps16310@fpt.edu.vn>
2022-12-11 19:10:30 +0700

    fix

--- resources/views/home/pages/home.blade.php
+++ resources/views/home/pages/home.blade.php
@@ -4,35 +4,23 @@
 
 <div class="page--home">
 
-    <div style="display:grid;    grid-auto-columns: 70% 1fr; gap: 10px; margin-bottom: 50px">
+    <div style="display:grid;    grid-template-columns: 70% 1fr; gap: 10px; margin-bottom: 50px">
         <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
             <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                @foreach($coupons as $key => $coupon)
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="{{$key}}" class="{{$key == 0 ? 'active' : ''}}" aria-current="{{$key == 0 ? 'true' : 'false'}}" aria-label="Slide 1"></button>
+                @endforeach
             </div>
             <div class="carousel-inner">
-                <div class="carousel-item active" data-bs-interval="10000">
-                    <img src="http://shopeeplus.com//upload/images/e-voucher-shopee.jpg" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
+                @foreach($coupons as $coupon)
+                <div class="carousel-item active" data-bs-interval="5000">
+                    <img src="{{asset('upload/voucher/'.$coupon->avatar)}}" style="width: 100%; height: 300px; object-fit:cover" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block" style="color:white">
+                        <h2 style="  text-shadow: 0px 2px 2px rgba(255, 255, 255, 0.4);  ">{{$coupon->name}}</h2>
+                        <p style="  text-shadow: 0px 2px 2px rgba(255, 255, 255, 0.4); ">{{$coupon->message}}</p>
                     </div>
                 </div>
-                <div class="carousel-item" data-bs-interval="2000">
-                    <img src="https://cf.shopee.vn/file/789f4f5f4e5d9d52049674b4bed1bd3f" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Second slide label</h5>
-                        <p>Some representative placeholder content for the second slide.</p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="https://congnghe247.info/wp-content/uploads/2022/03/ma-giam-gia-shopee-15-3-2022.png" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Third slide label</h5>
-                        <p>Some representative placeholder content for the third slide.</p>
-                    </div>
-                </div>
+                @endforeach
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -43,8 +31,13 @@
                 <span class="visually-hidden">Next</span>
             </button>
         </div>
-        <div class="right">
-
+        <div style="display: grid;grid-template-rows: 48% 48%; height:300px;align-content: space-between">
+            <div class="img" style="width: 100%;height:100%">
+                <img src="http://shopeeplus.com//upload/images/e-voucher-shopee.jpg" style="width: 100%;height:100%; object-fit:cover" alt="...">
+            </div>
+            <div class="img" style="width: 100%;height:100%">
+                <img src="http://shopeeplus.com//upload/images/e-voucher-shopee.jpg" style="width: 100%;height:100%; object-fit:cover" alt="...">
+            </div>
         </div>
     </div>
 
