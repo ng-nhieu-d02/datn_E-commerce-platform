007ed1119ad3721611284e902ef197a8e9c98e48
nguyennhieu1507 <nhieunxps16310@fpt.edu.vn>
2022-12-11 18:29:08 +0700

    fix config

--- resources/views/home/layout/ingredient/head.blade.php
+++ resources/views/home/layout/ingredient/head.blade.php
@@ -47,4 +47,3 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.css"
     integrity="sha512-xmGTNt20S0t62wHLmQec2DauG9T+owP9e6VU8GigI0anN7OXLip9i7IwEhelasml2osdxX71XcYm6BQunTQeQg=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
-
