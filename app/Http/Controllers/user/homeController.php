d5d35f1523627da786aea1a209f7f061eae56ef2
phatlx149 <phatle1913@gmail.com>
2022-12-15 10:59:16 +0700

    Phat push code filter

--- app/Http/Controllers/user/homeController.php
+++ app/Http/Controllers/user/homeController.php
@@ -23,35 +23,35 @@ class homeController extends Controller
     {
         // product bình thường
         $products = Product::select('product.*')->join('store', 'store.id', '=', 'product.id_store')->where('store.status', '=', '1')
-        ->where('product.status', '=', '0')->paginate(15);
+            ->where('product.status', '=', '0')->paginate(15);
 
         // product bán chạy nhất
         $product_hotSale = Product::select('product.*', DB::raw('sum(product_detail.sold) as sold'))
-        ->join('product_detail', 'product_detail.id_product','=','product.id')
-        ->join('store', 'store.id' ,'=', 'product.id_store')
-        ->where('store.status', '=', '1')
-        ->where('product.status', '=', '0')
-        ->groupBy('product_detail.id_product')
-        ->orderBy('sold', 'DESC')
-        ->paginate(15);
+            ->join('product_detail', 'product_detail.id_product', '=', 'product.id')
+            ->join('store', 'store.id', '=', 'product.id_store')
+            ->where('store.status', '=', '1')
+            ->where('product.status', '=', '0')
+            ->groupBy('product_detail.id_product')
+            ->orderBy('sold', 'DESC')
+            ->paginate(15);
 
         // sản phẩm nhiều lượt xem nhất
-        $product_hotView = Product::select('product.*')->join('store', 'store.id' ,'=', 'product.id_store')
-        ->where('store.status', '=', '1')
-        ->where('product.status', '=', '0')
-        ->orderBy('product.view', 'DESC')
-        ->paginate(15);
+        $product_hotView = Product::select('product.*')->join('store', 'store.id', '=', 'product.id_store')
+            ->where('store.status', '=', '1')
+            ->where('product.status', '=', '0')
+            ->orderBy('product.view', 'DESC')
+            ->paginate(15);
 
         // sản phẩm được thuê up top
-        $product_upTop = Product::select('product.*')->join('store', 'store.id' ,'=', 'product.id_store')
-        ->where('store.status', '=', '1')
-        ->where('product.status', '=', '0')
-        ->where('product.view_prioritized', '>', 0)
-        ->inRandomOrder()
-        ->paginate(15);
+        $product_upTop = Product::select('product.*')->join('store', 'store.id', '=', 'product.id_store')
+            ->where('store.status', '=', '1')
+            ->where('product.status', '=', '0')
+            ->where('product.view_prioritized', '>', 0)
+            ->inRandomOrder()
+            ->paginate(15);
 
         // sự kiện khuyến mãi
-        $coupons = Coupons::where(['apply_store' => '0' , 'status' => '0', 'coupon_type' => '0'])->where('stop_time','>=',Carbon::now('Asia/Ho_Chi_Minh')->toDateTime())->where('start_time','<=',Carbon::now('Asia/Ho_Chi_Minh')->toDateTime())->paginate(10);
+        $coupons = Coupons::where(['apply_store' => '0', 'status' => '0', 'coupon_type' => '0'])->where('stop_time', '>=', Carbon::now('Asia/Ho_Chi_Minh')->toDateTime())->where('start_time', '<=', Carbon::now('Asia/Ho_Chi_Minh')->toDateTime())->paginate(10);
 
         $product = Product::paginate(9);
         return view('home.pages.home', [
@@ -67,11 +67,16 @@ class homeController extends Controller
 
         $getCategoryProductChildren = CategoryProduct::where("parent_id", "<>", 0)->get();
 
+        $getAllColor = ProductDetail::select("color_value")->distinct()->get();
+
+        $getAllAttribute = ProductDetail::select("attribute_value")->distinct()->get();
 
         return view('home.pages.pageSearch', [
             'product' => $product,
             'getAllCategoryProducts' => $getAllCategoryProducts,
             'getCategoryProductChildren' => $getCategoryProductChildren,
+            'getAllColor' => $getAllColor,
+            'getAllAttribute' => $getAllAttribute,
         ]);
     }
 
@@ -94,6 +99,27 @@ class homeController extends Controller
         ]);
     }
 
+    public function filterProduct(Request $request)
+    {
+        $queryFilter = Product::with('comment')
+            ->join("product_detail", "product_detail.id", "=", "product.id");
+        if (isset($request->arrayCategories)) {
+            $queryFilter->whereIn("product.category_id", $request->arrayCategories);
+        }
+        if (isset($request->arrayAttributes)) {
+            $queryFilter->whereIn("product_detail.attribute_value", $request->arrayAttributes);
+        }
+        if (isset($request->inputMin) && isset($request->inputMax)) {
+            $queryFilter->whereBetween("product_detail.price", array($request->inputMin, $request->inputMax));
+        }
+
+        $data = $queryFilter->get();
+        return response()->json([
+            'status' => true,
+            'data' => $data,
+        ]);
+    }
+
 
     public function lucky()
     {
@@ -108,4 +134,3 @@ class homeController extends Controller
         $product->save();
     }
 }
-
--- resources/views/home/pages/pageSearch.blade.php
+++ resources/views/home/pages/pageSearch.blade.php
@@ -76,18 +76,18 @@
                                         <div class="progress"></div>
                                     </div>
                                     <div class="range--input">
-                                        <input type="range" class="range-min" min="0" max="10000" value="2500" step="100">
-                                        <input type="range" class="range-max" min="0" max="10000" value="7500" step="100">
+                                        <input type="range" class="range-min" min="0" max="1000000" value="250000" step="100">
+                                        <input type="range" class="range-max" min="0" max="1000000" value="750000" step="100">
                                     </div>
                                     <div class="price--input">
                                         <div class="field">
                                             <span>Min</span>
-                                            <input type="number" class="input-min" value="2500">
+                                            <input type="number" class="input-min" value="250000">
                                         </div>
                                         <div class="separator">-</div>
                                         <div class="field">
                                             <span>Max</span>
-                                            <input type="number" class="input-max" value="7500">
+                                            <input type="number" class="input-max" value="750000">
                                         </div>
                                     </div>
                                 </div>
@@ -117,7 +117,7 @@
                                     </div>
                                     @foreach ($getCategoryProductChildren as $cateProductChild) 
                                     <div class="form-check flex items-center">
-                                        <input class="form-check-input mt-0 children" id="flexRadioDefault{{ $cateProductChild->id }}" type="checkbox" value="{{ $cateProductChild->id }}" aria-label="Checkbox for following text input">
+                                        <input class="form-check-input mt-0 children" id="flexRadioDefault{{ $cateProductChild->id }}" type="checkbox" name="cate_product[]" value="{{ $cateProductChild->id }}" aria-label="Checkbox for following text input">
                                         <label class="form-check-label px-3" for="flexRadioDefault{{ $cateProductChild->id }}">{{ $cateProductChild->name }}</label>
                                     </div>
                                     @endforeach
@@ -181,34 +181,12 @@
                             </button>
                             <div class="dropdown-menu text-base btn--size--show rounded--1em w--screen w--20">
                                 <div class="relative flex flex-col px-4 py-4 space-y-5">
-                                    <div class="form-check flex items-center">
-                                        <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
-                                        <label class="form-check-label px-3" for="flexRadioDefault1">XXS</label>
-                                    </div>
-                                    <div class="form-check flex items-center">
-                                        <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
-                                        <label class="form-check-label px-3" for="flexRadioDefault1">XS</label>
-                                    </div>
-                                    <div class="form-check flex items-center">
-                                        <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
-                                        <label class="form-check-label px-3" for="flexRadioDefault1">S</label>
-                                    </div>
-                                    <div class="form-check flex items-center">
-                                        <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
-                                        <label class="form-check-label px-3" for="flexRadioDefault1">M</label>
-                                    </div>
-                                    <div class="form-check flex items-center">
-                                        <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
-                                        <label class="form-check-label px-3" for="flexRadioDefault1">L</label>
-                                    </div>
-                                    <div class="form-check flex items-center">
-                                        <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
-                                        <label class="form-check-label px-3" for="flexRadioDefault1">XL</label>
-                                    </div>
-                                    <div class="form-check flex items-center">
-                                        <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
-                                        <label class="form-check-label px-3" for="flexRadioDefault1">2XL</label>
-                                    </div>
+                                  @foreach($getAllAttribute as $attribute)
+                                  <div class="form-check flex items-center">
+                                    <input class="form-check-input mt-0" name="attribute[]" type="checkbox" value="{{ $attribute->attribute_value }}" aria-label="Checkbox for following text input">
+                                    <label class="form-check-label px-3" for="flexRadioDefault1">{{ $attribute->attribute_value }}</label>
+                                </div>
+                                  @endforeach
                                 </div>
                                 <div class="p-4 flex items-center justify-between">
                                     <button class="btn rounded-full text-base lb--check btn--bg--light btn--sort--clear relative inline-flex items-center justify-center rounded-full ">Clear</button>
@@ -217,7 +195,7 @@
                             </div>
                         </div>
                         <div class="btn-group">
-                            <button type="button" class="btn text-base btn--sub--filter rounded-full lb--check flex items-center justify-center" data-bs-toggle="dropdown" aria-expanded="false">
+                            <button type="button" id="onSale" class="btn text-base btn--sub--filter rounded-full lb--check flex items-center justify-center" data-bs-toggle="dropdown" aria-expanded="false">
                                 <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M3.9889 14.6604L2.46891 13.1404C1.84891 12.5204 1.84891 11.5004 2.46891 10.8804L3.9889 9.36039C4.2489 9.10039 4.4589 8.59038 4.4589 8.23038V6.08036C4.4589 5.20036 5.1789 4.48038 6.0589 4.48038H8.2089C8.5689 4.48038 9.0789 4.27041 9.3389 4.01041L10.8589 2.49039C11.4789 1.87039 12.4989 1.87039 13.1189 2.49039L14.6389 4.01041C14.8989 4.27041 15.4089 4.48038 15.7689 4.48038H17.9189C18.7989 4.48038 19.5189 5.20036 19.5189 6.08036V8.23038C19.5189 8.59038 19.7289 9.10039 19.9889 9.36039L21.5089 10.8804C22.1289 11.5004 22.1289 12.5204 21.5089 13.1404L19.9889 14.6604C19.7289 14.9204 19.5189 15.4304 19.5189 15.7904V17.9403C19.5189 18.8203 18.7989 19.5404 17.9189 19.5404H15.7689C15.4089 19.5404 14.8989 19.7504 14.6389 20.0104L13.1189 21.5304C12.4989 22.1504 11.4789 22.1504 10.8589 21.5304L9.3389 20.0104C9.0789 19.7504 8.5689 19.5404 8.2089 19.5404H6.0589C5.1789 19.5404 4.4589 18.8203 4.4589 17.9403V15.7904C4.4589 15.4204 4.2489 14.9104 3.9889 14.6604Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                                     <path d="M9 15L15 9" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
@@ -226,6 +204,9 @@
                                 </svg>
                                 <span class="ms-3">On Sale</span>
                             </button>
+                        </div>
+                        <div class="btn-group">
+                            <button type="button" id="filter">Lọc</button>
                         </div>
                     </div>
                     <div class="block flex-shrink-0 text-right">
@@ -469,8 +450,9 @@
 @endsection
 @section("scripts")
 <script>
-    $(document).ready(function(){
+    $(document).ready(async function(){
         $(".btn-check").click(function(){
+            console.log(1);
             let CategoryProductParentId = $(this).val();
 
             $.ajax({
@@ -492,7 +474,7 @@
                             response.data.forEach(function(value, key) {
                             checkboxEl += `
                             <div class="form-check flex items-center">
-                                <input class="form-check-input mt-0" type="checkbox" value="${value.id}" aria-label="Checkbox for following text input">
+                                <input class="form-check-input mt-0 children" name="cate_product[]" type="checkbox" value="${value.id}" aria-label="Checkbox for following text input">
                                 <label class="form-check-label px-3" for="">${value.name}</label>
                             </div>`;
                             })
@@ -507,36 +489,175 @@
                 }
             });
 
+            
+        });
+        $(document).on("change", "#check-all", function(){
+            console.log(3)
+            if (this.checked) {
+                $(".children").each(function() {
+                    this.checked=true;
+                });
+            } else {
+                $(".children").each(function() {
+                    this.checked=false;
+                });
+            }
+
+            $(".children").click(function () {
+                if ($(this).is(":checked")) {
+                    var isAllChecked = 0;
+
+                    $(".children").each(function() {
+                        if (!this.checked)
+                            isAllChecked = 1;
+                    });
+
+                    if (isAllChecked == 0) {
+                        $("#check-all").prop("checked", true);
+                    }     
+                }
+                else {
+                    $("#check-all").prop("checked", false);
+                }
+            });
         })
-        $("#check-all").change(function() {
-        if (this.checked) {
-            $(".children").each(function() {
-                this.checked=true;
-            });
-        } else {
-            $(".children").each(function() {
-                this.checked=false;
-            });
-        }
-    });
 
-    $(".children").click(function () {
-        if ($(this).is(":checked")) {
-            var isAllChecked = 0;
-
-            $(".children").each(function() {
-                if (!this.checked)
-                    isAllChecked = 1;
+        const rangeInput = document.querySelectorAll(".range--input input"),
+        priceInput = document.querySelectorAll(".price--input input"),
+        range = document.querySelector(".price--slider .progress");
+        let priceGap = 1000;
+        priceInput.forEach(input =>{
+            input.addEventListener("input", e =>{
+                let minPrice = parseInt(priceInput[0].value),
+                maxPrice = parseInt(priceInput[1].value);
+                
+                if((maxPrice - minPrice >= priceGap) && maxPrice <= rangeInput[1].max){
+                    if(e.target.className === "input-min"){
+                        rangeInput[0].value = minPrice;
+                        range.style.left = ((minPrice / rangeInput[0].max) * 100) + "%";
+                    }else{
+                        rangeInput[1].value = maxPrice;
+                        range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+                    }
+                }
             });
+        });
+        rangeInput.forEach(input =>{
+            input.addEventListener("input", e =>{
+                let minVal = parseInt(rangeInput[0].value),
+                maxVal = parseInt(rangeInput[1].value);
+                if((maxVal - minVal) < priceGap){
+                    if(e.target.className === "range-min"){
+                        rangeInput[0].value = maxVal - priceGap
+                    }else{
+                        rangeInput[1].value = minVal + priceGap;
+                    }
+                }else{
+                    priceInput[0].value = minVal;
+                    priceInput[1].value = maxVal;
+                    range.style.left = ((minVal / rangeInput[0].max) * 100) + "%";
+                    range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+                }
+            });
+        });
+        function formatCurrency(money){
+           return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(money * 1)
+        }
 
-            if (isAllChecked == 0) {
-                $("#check-all").prop("checked", true);
-            }     
-        }
-        else {
-            $("#check-all").prop("checked", false);
-        }
-    });
+        $("#filter").click(function(){
+            let arrayCategories = [];
+            let arrayAttributes = [];
+
+            let inputMin = $("input.input-min").val();
+            let inputMax = $("input.input-max").val();
+
+            let checkOnSale = false;
+            $("#onSale").click(function(){
+                checkOnSale = true;
+            })
+            let arrayColors = [];
+
+            // category = $("input[type=radio].btn-check:checked").val();
+
+            $('input[name="cate_product[]"]:checked').each(function(i, obj){
+                arrayCategories.push(obj.value)
+            })
+
+            $('input[name="attribute[]"]:checked').each(function(i, obj){
+                arrayAttributes.push(obj.value)
+            })
+
+            console.log(arrayCategories, arrayAttributes);
+            $.ajax({
+                url : "{{ route('filter_product') }}",
+                data: {
+                    "arrayCategories" : arrayCategories,
+                    "arrayAttributes" : arrayAttributes,
+                    "inputMin" : inputMin,
+                    "inputMax" : inputMax,
+                },
+                type: "get",
+                dataType: "json",
+                success: function(response){
+                    console.log(response);
+                    if(response.status){
+                        let cardProductEl = ``;
+                       if(response.data.length > 0){
+                            response.data.forEach(function(item, key)  {
+                                let sum = 0;
+                                let sumCommentProduct = item.comment.filter(comment => {
+                                    sum+= comment.rate;
+                                })
+
+                                let resultSum = 0;
+                                if(sum > 0){
+                                    resultSum = sum / item.comment.length
+                                }
+                                console.log(item);
+                                cardProductEl += `
+                                <div class="component--cardProduct">
+                                    <div class="component--cardProduct--img">
+                                        <a href="http://127.0.0.1:8000/product/rey-nylon-backpack-1668402744">
+                                            <img class="image-product" src="{{ asset("upload/product/`+item.thumb+`") }}" alt="">
+                                        </a>
+                                        <i class="fa-regular fa-heart btn-add-wishlist true" data-id="1"></i>
+                                    </div>
+                                    <div class="component--cardProduct--content">
+                                        <a href="http://127.0.0.1:8000/product/rey-nylon-backpack-1668402744">
+                                            <p class="title"> ${item.name} </p>
+                                            <p class="der"> ${item.description} </p>
+                                            <div class="d-flex justify-content-between div--content">
+                                                <div class="left">
+                                                    <i class="fa-solid fa-star"></i>
+                                                    <span>
+                                                        ${resultSum != 0 ? resultSum +" ("+ item.comment.length + ")"   : 'Chưa có đánh giá' } 
+                                                        
+                                                    </span>
+                                                </div>
+                                                <p class="m-0">${item.sold > 0 ? 'Đã bán '+item.sold : ''}</p>
+                                            </div>
+                                            <p class="price"><span>${formatCurrency(item.price)}</span> <span>-10%</span></p>
+                                        </a>
+                                    </div>
+                                </div>
+                                `;
+                            })
+                            $(".page--home--product").html(cardProductEl);
+                       }else{
+                        $(".page--home--product").html("Không có sản phẩm phù hợp");
+                       }
+                    }
+                },
+                error: function(error){
+
+                }
+                
+            })
+
+        })
+
+        
+        
     })
 </script>
 @endsection
